063cc59bb16c8e519474e377eb9df6781ffe0a16
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-02-19 08:20:08 +0000

    added constructor and corrected mistakes

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 class Person:
   height = 153
   age = 12
-  name = "zakhar"
+  name = "Zakhar"
   is_male = True
+
+#це конструктор
+#  ця вункція запускається при створенні
 
 def __init__(self, surname):
   self.surname = Rubish
   print(self)
 
-  
-me = Person(zakhar)
-you = Person()
+# створюємо класс (екземпляр класу)
+me = Person("Rubish")
+# тут також
+you = Person("test")
 
-print(me.age)
-me.age += 1
-print(me.age)
+# print(me.surname)
+# print(you.surname)
+
+
 
