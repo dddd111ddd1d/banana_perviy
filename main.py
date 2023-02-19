08aff624d657197309c331d7b4a785d0d403e4d1
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-02-19 08:38:30 +0000

    added hobby

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ class Person:
   age = 12
   name = "Zakhar"
   is_male = True
+  hobby = "Learning Python"
 
 #це конструктор
 #  ця вункція запускається при створенні
 
 def __init__(self, surname):
   self.surname = Rubish
-  print(self)
+  print(self.Zakhar)
+  
+
+def do_my_thing(self):
+  print("I love", self.hobby)
+
+  
 
 # створюємо класс (екземпляр класу)
 me = Person("Rubish")
@@ -18,6 +25,3 @@ you = Person("test")
 
 # print(me.surname)
 # print(you.surname)
-
-
-
