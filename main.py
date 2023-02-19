85354c419aae765cf4fbb9eb4c94bf1f2f94a1f8
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-02-19 08:13:11 +0000

    added  self surname

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 class Person:
   height = 153
   age = 12
-  name = "banana"
+  name = "zakhar"
   is_male = True
 
+def __init__(self, surname):
+  self.surname = Rubish
+  print(self)
 
-me = Person()
+  
+me = Person(zakhar)
 you = Person()
 
 print(me.age)
 me.age += 1
 print(me.age)
+
