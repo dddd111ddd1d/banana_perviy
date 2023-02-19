c466d5665873632637ffcdc854c709a24f15c89a
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-02-19 08:54:10 +0000

    added my friend hobby

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 class Person:
-  height = 153
-  age = 12
-  name = "Zakhar"
-  is_male = True
-  hobby = "Learning Python"
-
-#це конструктор
-#  ця вункція запускається при створенні
-
+    height = 153
+    age = 12
+    name = "Zakhar"
+    is_male = True
+    hobby = "Learning Python"
+  
+  #це конструктор
+  #  ця вункція запускається при створенні
+  
 def __init__(self, surname):
-  self.surname = Rubish
-  print(self.Zakhar)
-  
-
+    self.surname = surname
+    print(self.Zakhar)
+      
+    
 def do_my_thing(self):
-  print("I love", self.hobby)
-
+    print("I love", self.hobby)
   
-
+    
+  
 # створюємо класс (екземпляр класу)
 me = Person("Rubish")
-# тут також
-you = Person("test")
+  
+me.hobby = "banana"
+me.do_my_thing()
+  
+my_friend = Person("olegrix")
 
-# print(me.surname)
-# print(you.surname)
+my_friend.hobby = "самакат"
+  
